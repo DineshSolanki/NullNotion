05b7e59ca9aba87c98a2ca2a95c8042434aad85e
DineshSolanki <15937452+DineshSolanki@users.noreply.github.com>
2023-01-22 16:11:00 +0530

    wip [NOT RELEASE READY]

--- src/main/java/com/abstractprogrammer/nullnotion/actions/ProcessNullNotionAction.java
+++ src/main/java/com/abstractprogrammer/nullnotion/actions/ProcessNullNotionAction.java
@@ -5,13 +5,21 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.UpdateInBackground;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.project.IndexNotReadyException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiManager;
+import org.apache.commons.collections.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class ProcessNullNotionAction extends AnAction {
@@ -23,30 +31,50 @@ public class ProcessNullNotionAction extends AnAction {
 
     @Override
     public void update(@NotNull AnActionEvent e) {
-        logger.info("Update method called");
-        Project project = e.getProject();
-        Presentation presentation = e.getPresentation();
-        VirtualFile selectedFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-        if (project == null) {
-            logger.warn("Project is null");
-            presentation.setEnabledAndVisible(false);
-        } else if (selectedFile == null || !selectedFile.getName().endsWith(".java")) {
-            logger.warn("Selected file is not a java file");
-            presentation.setEnabledAndVisible(false);
-        } else {
-            psiJavaFile = (PsiJavaFile) PsiManager.getInstance(project).findFile(selectedFile);
-            if (psiJavaFile == null) {
-                logger.warn("could not find psi file");
-                presentation.setEnabledAndVisible(false);
-            } else {
-                selectedClass = psiJavaFile.getClasses()[0];
-                if (selectedClass == null) {
-                    logger.warn("could not find any class in the file");
+        Application application = ApplicationManager.getApplication();
+        try {
+            application.executeOnPooledThread(() -> {
+                logger.info("Update method called");
+                Project project = e.getProject();
+                Presentation presentation = e.getPresentation();
+                VirtualFile selectedFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
+                if (project == null) {
+                    logger.warn("Project is null");
+                    presentation.setEnabledAndVisible(false);
+                } else if (selectedFile == null || !selectedFile.getName().endsWith(".java")) {
+                    logger.warn("Selected file is not a java file");
                     presentation.setEnabledAndVisible(false);
                 } else {
-                    presentation.setEnabledAndVisible(selectedClass.getAnnotation(ENTITY_ANNOTATION) != null);
+                    if (!DumbService.isDumb(project)) {
+                        ReadAction.run(() ->{
+                            psiJavaFile = (PsiJavaFile) PsiManager.getInstance(project).findFile(selectedFile);
+                        });
+                        if (psiJavaFile == null) {
+                            logger.warn("could not find psi file");
+                            presentation.setEnabledAndVisible(false);
+                        } else {
+                            ReadAction.run(() ->{
+                                PsiClass[] classes = psiJavaFile.getClasses();
+                                if (classes.length > 0) {
+                                    selectedClass = classes[0];
+                                }
+                            });
+                            if (selectedClass == null) {
+                                logger.warn("could not find any class in the file");
+                                presentation.setEnabledAndVisible(false);
+                            } else {
+                                ReadAction.run(() -> {
+                                    PsiAnnotation entityAnnotation = selectedClass.getAnnotation(ENTITY_ANNOTATION);
+                                    presentation.setEnabledAndVisible(entityAnnotation != null);
+                                });
+                            }
+                        }
+                    }
                 }
-            }
+            });
+        } catch (IndexNotReadyException ex) {
+            logger.warn("Index is not ready, disable action");
+            e.getPresentation().setEnabledAndVisible(false);
         }
     }
 
