1566be37a6074007b0a30ac9c49a6b37514b453a
DineshSolanki <15937452+DineshSolanki@users.noreply.github.com>
2023-01-22 17:12:06 +0530

    Update optimization done, can be optimized more

--- src/main/java/com/abstractprogrammer/nullnotion/actions/ProcessNullNotionAction.java
+++ src/main/java/com/abstractprogrammer/nullnotion/actions/ProcessNullNotionAction.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.actionSystem.UpdateInBackground;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
@@ -15,11 +14,9 @@ import com.intellij.openapi.project.IndexNotReadyException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiManager;
-import org.apache.commons.collections.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class ProcessNullNotionAction extends AnAction {
@@ -32,49 +29,15 @@ public class ProcessNullNotionAction extends AnAction {
     @Override
     public void update(@NotNull AnActionEvent e) {
         Application application = ApplicationManager.getApplication();
+        Presentation presentation = e.getPresentation();
         try {
-            application.executeOnPooledThread(() -> {
-                logger.info("Update method called");
-                Project project = e.getProject();
-                Presentation presentation = e.getPresentation();
-                VirtualFile selectedFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-                if (project == null) {
-                    logger.warn("Project is null");
-                    presentation.setEnabledAndVisible(false);
-                } else if (selectedFile == null || !selectedFile.getName().endsWith(".java")) {
-                    logger.warn("Selected file is not a java file");
-                    presentation.setEnabledAndVisible(false);
-                } else {
-                    if (!DumbService.isDumb(project)) {
-                        ReadAction.run(() ->{
-                            psiJavaFile = (PsiJavaFile) PsiManager.getInstance(project).findFile(selectedFile);
-                        });
-                        if (psiJavaFile == null) {
-                            logger.warn("could not find psi file");
-                            presentation.setEnabledAndVisible(false);
-                        } else {
-                            ReadAction.run(() ->{
-                                PsiClass[] classes = psiJavaFile.getClasses();
-                                if (classes.length > 0) {
-                                    selectedClass = classes[0];
-                                }
-                            });
-                            if (selectedClass == null) {
-                                logger.warn("could not find any class in the file");
-                                presentation.setEnabledAndVisible(false);
-                            } else {
-                                ReadAction.run(() -> {
-                                    PsiAnnotation entityAnnotation = selectedClass.getAnnotation(ENTITY_ANNOTATION);
-                                    presentation.setEnabledAndVisible(entityAnnotation != null);
-                                });
-                            }
-                        }
-                    }
-                }
-            });
+            logger.info("Update method called");
+            Project project = e.getProject();
+            VirtualFile selectedFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
+            presentation.setEnabledAndVisible(shouldEnableAction(project, selectedFile));
         } catch (IndexNotReadyException ex) {
             logger.warn("Index is not ready, disable action");
-            e.getPresentation().setEnabledAndVisible(false);
+            presentation.setEnabledAndVisible(false);
         }
     }
 
@@ -87,6 +50,43 @@ public class ProcessNullNotionAction extends AnAction {
         } else {
             Messages.showErrorDialog("Could not find the class", "Error");
         }
+    }
+
+    /**
+
+     Determines if the action should be enabled based on the given project and selected file.
+     also sets the selectedClass and psiJavaFile fields
+     @param project the current project
+     @param selectedFile the currently selected file
+     @return true if the action should be enabled, false otherwise
+     */
+    private boolean shouldEnableAction(Project project, VirtualFile selectedFile) {
+        boolean isEnable = true;
+        if (project == null) {
+            logger.warn("Project is null");
+            isEnable = false;
+        } else if (selectedFile == null || !selectedFile.getName().endsWith(".java")) {
+            logger.warn("Selected file is not a java file");
+            isEnable = false;
+        } else if (!DumbService.isDumb(project)) {
+            psiJavaFile = ReadAction.compute(() -> (PsiJavaFile) PsiManager.getInstance(project).findFile(selectedFile));
+            if (psiJavaFile == null) {
+                logger.warn("could not find psi file");
+                isEnable = false;
+            } else {
+                PsiClass[] classes = ReadAction.compute(() -> psiJavaFile.getClasses());
+                if (classes.length > 0) {
+                    selectedClass = classes[0];
+                }
+                if (selectedClass == null) {
+                    logger.warn("could not find any class in the file");
+                    isEnable = false;
+                } else {
+                    isEnable = ReadAction.compute(() -> selectedClass.hasAnnotation(ENTITY_ANNOTATION));
+                }
+            }
+        }
+        return isEnable;
     }
 }
 
